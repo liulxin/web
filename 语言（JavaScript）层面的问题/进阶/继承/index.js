c8fa75911ab1f99a3cfd5a93894bceeea254659d
dongwudi <550293874@qq.com>
2019-10-11 22:50:17 +0800

    继承

--- 语言（JavaScript）层面的问题/进阶/继承/index.js
+++ 语言（JavaScript）层面的问题/进阶/继承/index.js
@@ -38,18 +38,126 @@
 // instance2.colors.push('pink');
 // console.log(instance2.colors) // ["red", "blue", "pink"]
 
-function SuperType() {
-  this.colors = ['red']
+// function SuperType() {
+//   this.colors = ['red']
+// }
+
+// function SubType() {
+//   SuperType.call(this)
+// }
+
+// var instance1 = new SubType()
+// instance1.colors.push('blue')
+// console.log(instance1.colors) // ["red", "blue"]
+
+// var instance2 = new SubType()
+// instance2.colors.push('pink')
+// console.log(instance2.colors) // ["red", "pink"]
+
+// function SuperType(name) {
+//   this.name = name
+//   this.colors = ['red', 'blue']
+// }
+// SuperType.prototype.sayName = function() {
+//   console.log(this.name)
+// }
+
+// function SubType(name, age) {
+//   SuperType.call(this, name)
+
+//   this.age = age
+// }
+
+// //继承方法
+// SubType.prototype = new SuperType()
+// SubType.prototype.constructor = SubType
+// SubType.prototype.sagAge = function() {
+//   console.log(this.age)
+// }
+
+// var instance1 = new SubType('nike', 20)
+// instance1.colors.push('pink')
+// console.log(instance1.colors) // ["red", "blue", "pink"]
+// instance1.sagAge() // 20
+// instance1.sayName() // nike
+
+// var instance2 = new SubType('gary', 21)
+// console.log(instance2.colors) // ["red", "blue"]
+// instance2.sagAge() // 21
+// instance2.sayName() // gary
+
+// function cObject(o) {
+//   function F(){}
+//   F.prototype = o;
+//   return new F()
+// }
+
+// var person = {
+//   name: 'nike',
+//   friends: ['van', 'court']
+// }
+
+// var anotherPerson = cObject(person);
+// anotherPerson.friends.push('super')
+
+// var yetPerson = cObject(person)
+// yetPerson.friends.push('arr')
+
+// console.log(person.friends) // ["van", "court", "super", "arr"]
+
+// function cObject(o) {
+//   function F() {}
+//   F.prototype = o
+//   return new F()
+// }
+
+// function createObj(o) {
+//   var clone = cObject(o)
+//   clone.sayName = function() {
+//     console.log('hi')
+//   }
+//   return clone
+// }
+
+// var person = {
+//   name: 'nike',
+//   friends: ['van', 'court']
+// }
+
+// var anotherPerson = createObj(person)
+// anotherPerson.sayName()
+
+function object(o) {
+  function F() {}
+  F.prototype = o
+  return new F()
 }
 
-function SubType() {
-  SuperType.call(this)
+function inheritPrototype(SubType, SuperType) {
+  var prototype = object(SuperType.prototype)
+  prototype.constructor = SubType
+  SubType.prototype = prototype
 }
 
-var instance1 = new SubType()
-instance1.colors.push('blue')
-console.log(instance1.colors) // ["red", "blue"]
+function SuperType(name) {
+  this.name = name
+  this.colors = ['red', 'blue']
+}
+SuperType.prototype.sayName = function() {
+  console.log(this.name)
+}
 
-var instance2 = new SubType()
-instance2.colors.push('pink')
-console.log(instance2.colors) // ["red", "pink"]
+function SubType(name, age) {
+  SuperType.call(this, name) // 第一次
+
+  this.age = age
+}
+
+inheritPrototype(SubType, SuperType)
+
+SubType.prototype.sagAge = function() {
+  console.log(this.age)
+}
+
+var instance1 = new SubType('nike', 20)
+instance1.sagAge()
